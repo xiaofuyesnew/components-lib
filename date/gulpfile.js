0dcd54e698ba4c12d0a0df22a8fc0eae3a12fa58
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-03-29 19:03:01 +0800

    new

--- date/gulpfile.js
+++ date/gulpfile.js
@@ -1,3 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
-const browserSync = require('browser-sync')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+
+gulp.task('sass', () => {
+    return gulp.src('src/style/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('dist/static/css'))
+})
+
+gulp.task('tpl', () => {
+    return gulp.src('src/page/*.html')
+        .pipe(gulp.dest('dist/'))
+})
