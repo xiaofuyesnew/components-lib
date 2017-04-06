636e4e5b5f233cf357c5cefd2e082d86db73ccd2
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-04-06 19:58:49 +0800

    move on

--- popup/src/script/popup.js
+++ popup/src/script/popup.js
@@ -8,17 +8,13 @@
  * 
  */
 
-var popup = {
-    
-}
-
 /**
  * @method Popup
  * @param {object} config 
  * @function
  *     弹出层对象构造函数
  */
-function Popup(config) {
+function popup(config) {
     var node = document.getElementById(config.node)
     console.log(node.offsetHeight)
     init(node)
@@ -28,4 +24,20 @@ function init(node) {
     if (node.style.display !== 'block') {
         node.style.display = 'block'
     }
+    var popup = node.nextElementSibling
+    console.log(popup)
+    popup.setAttribute('style', 'width:100px;height:100px;background-color: #e3e3e3;')
+    /*
+    document.styleSheets[0].addRule('.popup::before','color: green'); // 支持IE*/
+    document.styleSheets[0].insertRule('.popup::before {width:20px;height:20px;background-color: #e3e3e3;float:left;}', 0); // 支持非IE的现代浏览器
 }
+
+var config1 = {
+        node: "popup1"
+    },
+    config2 = {
+        node: "popup2"
+    }
+
+popup(config1)
+popup(config2)
