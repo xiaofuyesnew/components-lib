a8b49a49ac3e1945a84433a4efb0df5ed8b45df4
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-04-13 16:46:21 +0800

    NEW

--- popup/src/script/popup.js
+++ popup/src/script/popup.js
@@ -19,11 +19,14 @@ function popup(config) {
     //定义相关的变量
     var node = document.getElementById(config.node),    //主节点
         el = document.createElement(config.type.main),
+        elHeight = el.style.offset,
         mnbd = document.createElement('div'),   //要插入的主要元素
-        arrow = document.createElement('span'),
+        arrowBd = document.createElement('span'),
+        arrowBg = document.createElement('span'),
         def = {
-            bgColor: '#eee',
-            bdColor: '#ccc'
+            bgColor: "#eee",
+            bdColor: "#ccc",
+            arrowPosition: "20%"
         };
 
     //初始化外围
@@ -50,37 +53,67 @@ function popup(config) {
             }
             el.setAttribute('class', classContent);
         }
+
+        //注入input
         node.appendChild(el);
     }
+
 
     //主体配置
     mnbd.setAttribute('style', `
         position:absolute;
-        top:0;
+        top:${el.offsetHeight + 11}px;
         left:0;
         width:300px;
         height:200px;
         background-color: ${config.bgColor || def.bgColor};
         border:1px solid ${config.bdColor || def.bdColor};
         border-radius: 5px;
+        opacity:0;
+        z-index:1;
     `)
-    //注入及配置尖角
-    mnbd.appendChild(arrow);
+
+    //配置注入尖角
+    arrowBg.setAttribute('style', `
+        display:block;
+        width: 0;
+        height: 0;
+        float:left;
+        border-bottom: 10px solid ${config.bgColor || def.bgColor};
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        margin-top:-10px;
+        margin-left:${config.arrowPosition || def.arrowPosition};
+    `);
+    arrowBd.setAttribute('style', `
+        display:block;
+        width: 0;
+        height: 0;
+        float:left;
+        border-bottom: 10px solid ${config.bdColor || def.bdColor};
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        margin-top:-11px;
+        margin-left:${config.arrowPosition || def.arrowPosition};
+    `);
+    mnbd.appendChild(arrowBd);
+    mnbd.appendChild(arrowBg);
+
     //主体注入
     node.appendChild(mnbd);
+    
+    mnbdEvent(node);
 }
 
-function init(node) {
-    addType(node, )
+function mnbdEvent(node) {
+    node.firstChild.addEventListener('focus', (e) => {
+        e.target.nextElementSibling.style.opacity = '1';
+    }, true);
     /*
-    if (node.style.display !== 'block') {
-        node.style.display = 'block';
-    }
-    var popup = node.nextElementSibling;
-    var span = popup.getElementsByTagName('span');
-    popup.setAttribute('style', 'width:300px;height:200px;background-color: #eee;border-radius: 5px;border: 1px solid #ccc;');
-    span[1].setAttribute('style', 'display:block;width: 0;height: 0;float:left;border-bottom: 10px solid #eee;border-left: 10px solid transparent;border-right: 10px solid transparent;margin-top:-10px;margin-left:20%;')
-    span[0].setAttribute('style', 'display:block;width: 0;height: 0;float:left;border-bottom: 10px solid #ccc;border-left: 10px solid transparent;border-right: 10px solid transparent;margin-top:-11px;margin-left:20%;')
-
+    node.firstChild.addEventListener('blur', (e) => {
+        e.target.nextElementSibling.style.opacity = '0';
+    }, true);
     */
 }
+
+
